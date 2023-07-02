e8fa779fd56a564f5fd0d0fe3f19b2062c4617cc
Steven Almeroth <fedor@primesite.dev>
2023-07-02 13:59:57 -0400

    item

--- pages/home.ts
+++ pages/home.ts
@@ -1,12 +1,31 @@
 import { expect } from '@playwright/test'
 
-import type { Page } from '@playwright/test'
+import type { Locator, Page } from '@playwright/test'
 // import { isVisible } from '../framework/common-actions'
 
 const url = 'http://childcaresearch.ohio.gov/'
 
+type Item = {
+  name: string
+  address: string
+  city: string
+  zip: string
+  type: string
+  rating: number
+}
+
+async function scrape(row: Locator, css: string) {
+  const result = await row.locator(css).textContent() as string
+  return result.trim()
+}
+
+async function child(row: Locator, child: number) {
+  return await scrape(row, `div:nth-child(${child})`)
+}
+
 export class HomePage {
-  readonly page: Page;
+  readonly page: Page
+  public results: []
 
   constructor(page: Page) {
     this.page = page;
@@ -28,13 +47,17 @@ export class HomePage {
     const rows = await results.all()
     console.warn('Rows:', rows.length)
     await expect(results).toHaveCount(20)
-  }
-
-  // async userIsLoggedIn(): Promise<boolean> {
-  //     return await isVisible(this.page, 'a[routerlink="/editor"]');
-  // }
-
-  async goToSettings() {
-    await this.page.click('a[routerlink="/settings"]');
+    for (const row of rows) {
+      const stars = row.locator('div:nth-child(7) > .SUTQStarRating')
+      const item: Item = {
+        name: await child(row, 2),
+        address: await child(row, 3),
+        city: await child(row, 4),
+        zip: await child(row, 5),
+        type: await scrape(row, 'div:nth-child(6) > .desktopOnlyDisplay'),
+        rating: (await stars.all()).length,
+      }
+      console.log(row, item)
+    }
   }
 }
--- tests/childcaresearch.spec.ts
+++ tests/childcaresearch.spec.ts
@@ -1,18 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
-import { HomePage } from '../pages/home';
-
-function createAbsoluteUrl(base: string, relative: string): string {
-  const absoluteUrl = new URL(relative, base);
-  return absoluteUrl.href;
-}
+import { HomePage } from '../pages/home'
 
 test('child care search', async ({ page }) => {
-  const homepage = new HomePage(page);
+  const homepage = new HomePage(page)
   await homepage.open()
   await homepage.search()
-
-  // for (const row of rows) {
-  //   console.log(row)
-  // }
-});
+})
